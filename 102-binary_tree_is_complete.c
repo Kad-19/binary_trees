85956cc567b397f7c71c3c95e60c47fb9cf675fc
firacil <firaolmekbib511@gmail.com>
2024-03-27 12:43:40 -0700

    updated

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,6 +1,20 @@
 #include "binary_trees.h"
 
 /**
+ * _size - measure size of binary tree
+ * @tree: tree to be measured
+ * Return: size or NUll at fail
+ */
+
+size_t _size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (_size(tree->left) + _size(tree->right) + 1);
+}
+
+/**
  * binary_tree_is_complete - checks if binary tree is complete
  * @tree: pointer to the root node of the tree to check
  * Return: 1 or 0
@@ -8,42 +22,32 @@
 
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-	int flag = 0;
-	const binary_tree_t *stack[10000];
-	const binary_tree_t *node;
-	int top = -1;
+	size_t size;
 
 	if (!tree)
 		return (0);
+	size = _size(tree);
 
-	/* push root node to stack */
-	stack[++top] = tree;
+	return (_checker(tree, 0, size));
+}
 
-	/* lets perform depth-first traversal */
-	while (top >= 0)
-	{
-		node = stack[top--];
+/**
+ * _checker - checks if a binary tree is complete
+ * @tree: pointer to the root node of the tree to check
+ * @index: node index to check
+ * @size: number of nodes in the tree
+ *
+ * Return: 1 or 0
+ */
 
+int _checker(const binary_tree_t *tree, size_t index, size_t size)
+{
+	if (!tree)
+		return (1);
 
-		if (node == NULL)
-		{
-			flag = 1;
-		}
-		else
-		{
-			/**
-			 * if we encountered NUll node before, and current node is not null,
-			 * the tree is not complete
-			 */
-			if (flag)
-				return (0);
+	if (index >= size)
+		return (0);
 
-			/* push the right and left children onto stack */
-			stack[++top] = node->right;
-			stack[++top] = node->left;
-		}
-	}
-
-	/* if we reach here, tree is complete :) */
-	return (1);
+	return (_checker(tree->left, 2 * index + 1, size) &&
+		_checker(tree->right, 2 * index + 2, size));
 }
--- binary_trees.h
+++ binary_trees.h
@@ -49,5 +49,6 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 int _powr(int x, int y);
 void traverse(const binary_tree_t *tree, void (*func)(int), size_t level);
 int binary_tree_is_complete(const binary_tree_t *tree);
+int _checker(const binary_tree_t *tree, size_t index, size_t size);
 
 #endif /* _BINARY_TREES_H_ */
