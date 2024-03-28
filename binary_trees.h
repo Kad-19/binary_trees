36258c89146ccea1582f518a6ce9675d0dc5773e
firacil <firaolmekbib511@gmail.com>
2024-03-28 10:59:05 -0700

    updated

--- binary_trees.h
+++ binary_trees.h
@@ -50,7 +50,7 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 int _powr(int x, int y);
 void traverse(const binary_tree_t *tree, void (*func)(int), size_t level);
 int binary_tree_is_complete(const binary_tree_t *tree);
-int _checker(const binary_tree_t *tree, size_t index, size_t size);
+int _checker(const binary_tree_t *tree, int index, int size);
 int binary_tree_is_bst(const binary_tree_t *tree);
 int _is_bst(const binary_tree_t *tree, int min, int max);
 bst_t *bst_insert(bst_t **tree, int value);
