3e864e628513f26b5453747dcc156409895f5d11
firacil <firaolmekbib511@gmail.com>
2024-03-28 11:41:19 -0700

    updated

--- binary_trees.h
+++ binary_trees.h
@@ -58,5 +58,8 @@ bst_t *array_to_bst(int *array, size_t size);
 bst_t *bst_search(const bst_t *tree, int value);
 bst_t *bst_remove(bst_t *root, int value);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+					const binary_tree_t *second);
+
 
 #endif /* _BINARY_TREES_H_ */
